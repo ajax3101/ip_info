c02c64dd3f65b77a41f540be4114b2c8ebce2c2c
Denys Morozov <ajax3101@gmail.com>
2022-06-02 21:23:08 +0300

    ip info work

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
+from configparser import InterpolationMissingOptionError
 from urllib import request, response
 import requests
+from pyfiglet import Figlet
 
 
 def get_info_ip(ip='127.0.0.1'):
     try:
-        response = requests.get(url='http://ip-api.com/json/{ip}').json()
-        print(response)
+        response = requests.get(url=f'http://ip-api.com/json/{ip}').json()
+        # print(response)
+        data = {
+            '[IP]': response.get('query'),
+            '[ISP]': response.get('isp'),
+            '[Org]': response.get('org'),
+            '[Country]': response.get('country'),
+            '[Region Name]': response.get('regionName'),
+            '[City]': response.get('city'),
+            '[ZIP]': response.get('zip'),
+            '[Lat]': response.get('lat'),
+            '[Lon]': response.get('lon'),
+        }
+
+        for k, v in data.items():
+            print(f'{k} : {v}')
     except requests.exceptions.ConnectionError:
         print('[!] Please check your connection to Internet!')
 
 
 def main():
+    text = Figlet()
+    print(text.renderText('IP INFO'))
     ip = input('Please enter IP: ')
 
     get_info_ip(ip=ip)
